c7cb1c724fcdf926d595d6bd29ed5361f7f12ce6
Elton005 <eltonaliolivares@gmail.com>
2026-04-20 20:35:59 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v2.3-offline-fix';
+const CACHE_VERSION = 'v2.4-offline-fix';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `dynamic-${CACHE_VERSION}`;
 
